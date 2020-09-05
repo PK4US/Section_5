ed75359703d5e8f5f66c4ba5f6cf670d66fe5764
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-09-05 12:36:40 +0300

    Урок 8. Ползунок

--- Lesson_8/src/main/java/com/Lesson_8/MainActivity.java
+++ Lesson_8/src/main/java/com/Lesson_8/MainActivity.java
@@ -1,8 +1,9 @@
 package com.Lesson_8;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SeekBar sb = findViewById(R.id.seekBar);
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int progress = seekBar.getProgress();
+                System.out.println(progress);
+                TextView tv = findViewById(R.id.tv);
+                tv.setText("Прогресс: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                System.out.println("Нажат");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                System.out.println("Отпущен");
+            }
+        });
     }
 }
