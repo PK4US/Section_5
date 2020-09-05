c6fd42d343cd3c1b7173e2495703fd9698f6b264
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-09-05 13:17:00 +0300

    Урок 8. Ползунок из EditText в SeekBar

--- MyApp_8/src/main/java/com/MyApp_8/MainActivity.java
+++ MyApp_8/src/main/java/com/MyApp_8/MainActivity.java
@@ -1,8 +1,13 @@
 package com.MyApp_8;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final SeekBar sb = findViewById(R.id.seekBar);
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int progress = seekBar.getProgress();
+                System.out.println(progress);
+                TextView tv = findViewById(R.id.tv);
+                tv.setText("Ваш возраст: " + progress + " лет");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                System.out.println("Нажат");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                System.out.println("Отпущен");
+            }
+        });
+
+        Button button = findViewById(R.id.button);
+        final EditText et_number = findViewById(R.id.et_number);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num = Integer.parseInt(et_number.getText().toString());
+
+                sb.setProgress(num);
+            }
+        });
     }
 }
