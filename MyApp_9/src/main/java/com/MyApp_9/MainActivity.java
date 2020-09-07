4c8daf56b8a615bda710248761cd3797b0fcb113
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-09-07 13:54:45 +0300

    Урок 9. Вывести сообщение из editText в всплывающие сообщение

--- MyApp_9/src/main/java/com/MyApp_9/MainActivity.java
+++ MyApp_9/src/main/java/com/MyApp_9/MainActivity.java
@@ -3,6 +3,9 @@ package com.MyApp_9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void CallToast(View view) {
+        EditText et = findViewById(R.id.et_text);
+        String str = et.getText().toString();
+        Toast toast  = Toast.makeText(this, str,Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
