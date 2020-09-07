b9d61fc906e16fbb32276c24355a06b9ea057e3f
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-09-07 12:50:04 +0300

    Урок 9. Всплывающие сообщения

--- Lesson_9/src/main/java/com/Lesson_9/MainActivity.java
+++ Lesson_9/src/main/java/com/Lesson_9/MainActivity.java
@@ -3,6 +3,8 @@ package com.Lesson_9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void CallToast(View view) {
+        Toast toast  = Toast.makeText(this, "Hello",Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
