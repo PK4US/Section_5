058b4812306ba37b5d201d7b2bd36d2e533e668d
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-09-07 14:21:16 +0300

    Урок 10. Список

--- Lesson_10/src/main/java/com/Lesson_10/MainActivity.java
+++ Lesson_10/src/main/java/com/Lesson_10/MainActivity.java
@@ -3,12 +3,28 @@ package com.Lesson_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> beverages;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beverages = new ArrayList();
+        beverages.add("Сок");
+        beverages.add("Вода");
+        beverages.add("Газировка");
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1,beverages);
+
+        ListView lv = findViewById(R.id.LV);
+        lv.setAdapter(adapter);
     }
 }
