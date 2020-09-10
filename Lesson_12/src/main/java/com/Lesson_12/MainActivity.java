150f0e91f189bd00e92951bf00695570ab49911e
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-09-10 21:13:10 +0300

    Урок 12. Создание сложного списка

--- Lesson_12/src/main/java/com/Lesson_12/MainActivity.java
+++ Lesson_12/src/main/java/com/Lesson_12/MainActivity.java
@@ -1,8 +1,12 @@
 package com.Lesson_12;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(new Person("Алексей", 20));
+        persons.add(new Person("Екатерина", 25));
+        persons.add(new Person("Александр", 30));
+        persons.add(new Person("Евгений", 30));
+        persons.add(new Person("Татьяна", 30));
+
+        ListView lv = findViewById(R.id.lv);
+        PersonAdapter adapter = new PersonAdapter(this, R.layout.item, persons);
+
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Person p = (Person) adapterView.getItemAtPosition(position);
+                System.out.println("Вы выбрали: " + p.getName() + ", " + p.getAge() + " лет");
+            }
+        });
     }
 }
---
+++ Lesson_12/src/main/java/com/Lesson_12/PersonAdapter.java
@@ -0,0 +1,63 @@
+package com.Lesson_12;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import java.util.List;
+
+public class PersonAdapter extends ArrayAdapter<Person> {
+
+    private LayoutInflater lif;
+    private int resource;
+    private List<Person> persons;
+
+    public PersonAdapter(@NonNull Context context, int resource, List<Person> persons) {
+        super(context, resource, persons);
+        this.lif = LayoutInflater.from(context);
+        this.resource = resource;
+        this.persons = persons;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = lif.inflate(this.resource, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        else viewHolder = (ViewHolder) convertView.getTag();
+        final Person p = persons.get(position);
+        viewHolder.nameView.setText(p.getName());
+        viewHolder.ageView.setText("" + p.getAge());
+
+        viewHolder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                persons.remove(p);
+                notifyDataSetChanged();
+            }
+        });
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        final TextView nameView, ageView;
+        final Button button;
+
+        public ViewHolder(View view) {
+            nameView = view.findViewById(R.id.name);
+            ageView = view.findViewById(R.id.age);
+            button = view.findViewById(R.id.delete);
+        }
+
+    }
+
+}
